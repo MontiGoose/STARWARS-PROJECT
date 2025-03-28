54f518f7cb589d049d2b8dad1e72a36721ee71e6
MontiGoose <danielmonti.1997@gmail.com>
2025-03-28 15:11:07 +0000

    Add files via upload

---
+++ SerGalactico.java
@@ -0,0 +1,9 @@
+package lucasfilms;
+
+//Interfaz que define características comunes a todos los seres galácticos
+//-------------------------------------------------------------------------
+interface SerGalactico {
+ void mostrarInfo();
+ void hablarFraseIconica();
+ void realizarAccion();
+}
